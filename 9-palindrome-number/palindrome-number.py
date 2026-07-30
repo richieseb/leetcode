8261109d5b147ea3ca8fa6731dcf3d508624029c
richieseb <Ritsika.lenka@gmail.com>
2026-07-30 15:55:03 +0530

    Time: 15 ms (34.92%) | Memory: 12.5 MB (17.90%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        y=int(str(x)[::-1])
+        if x==y:
+            return True
+        else:
+            return False
