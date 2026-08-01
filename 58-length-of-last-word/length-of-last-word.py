a37afccbce0449fc33b11552b16e2a1636703389
richieseb <Ritsika.lenka@gmail.com>
2026-08-01 23:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.56%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count=0
+        s=s.split()
+        if len(s)==0:
+            return 0
+        else:
+            return len(s[-1]) 
