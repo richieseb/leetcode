a9550c225f61552ac5ed663ed2203d0f60acd66e
richieseb <Ritsika.lenka@gmail.com>
2026-08-21 20:48:13 +0530

    Time: 7 ms (69.16%) | Memory: 13.5 MB (73.86%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        counter={}
+        for num in nums:
+            if num in counter:
+                counter[num]+=1
+            else:
+                counter[num]=1
+        max_count=-1
+        ans=-1
+        for key,val in counter.items():
+            if val>max_count:
+                max_count=val
+                ans=key
+        return ans
+        
