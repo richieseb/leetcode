e2cad4e9c77376bfaeecb7d62321fc016656c236
richieseb <Ritsika.lenka@gmail.com>
2026-08-21 20:54:52 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (39.24%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,16 +1,13 @@
 class Solution(object):
     def majorityElement(self, nums):
-        counter={}
-        for num in nums:
-            if num in counter:
-                counter[num]+=1
-            else:
-                counter[num]=1
-        max_count=-1
         ans=-1
-        for key,val in counter.items():
-            if val>max_count:
-                max_count=val
-                ans=key
+        count=0
+        for num in nums:
+            if count==0:
+                ans=num
+            if ans==num:
+                count+=1
+            else:
+                count-=1
         return ans
         
