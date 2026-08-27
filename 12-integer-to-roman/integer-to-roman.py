8274d8ad8ce64f50d5c164387da2b636837319a2
richieseb <Ritsika.lenka@gmail.com>
2026-08-27 23:23:02 +0530

    Time: 5 ms (66.66%) | Memory: 12.3 MB (92.05%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        val_map = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+
+        roman = []
+        for val, symbol in val_map:
+            if num == 0:
+                break
+            count, num = divmod(num, val)
+            roman.append(symbol * count)
+
+        return "".join(roman)
