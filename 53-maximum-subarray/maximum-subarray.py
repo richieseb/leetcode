4f49c730f7b6774360b1ea659bf150898bc57388
richieseb <Ritsika.lenka@gmail.com>
2026-09-18 11:20:40 +0530

    Time: 55 ms (81.87%) | Memory: 22.1 MB (11.00%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxsub=nums[0]
+        cursum=0
+        for i in nums:
+            if cursum<0:
+                cursum=0
+            cursum+=i
+            maxsub=max(maxsub,cursum)
+        return maxsub
