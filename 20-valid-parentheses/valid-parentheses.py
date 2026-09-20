b4d1b9898ed0d6c07850fcc66e8937cc11118b4a
richieseb <Ritsika.lenka@gmail.com>
2026-09-20 19:36:30 +0530

    Time: 3 ms (73.70%) | Memory: 12.5 MB (73.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,26 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        #STACK
-        l1=['(','{','[']
-        l2=[')','}',']']
-        d=dict(zip(l2,l1))
-        if s[0] in l2 or s[-1] in l1:
-            return False
-
-        ind=0
-        result=[]
-        while ind<len(s):
-            if s[ind] in l1:
-                result.append(s[ind])
+        hashmap={')':'(','}':'{',']':'['}
+        stack=[]
+        for c in s:
+            if c not in hashmap:
+                stack.append(c)
             else:
-                n=d[s[ind]]
-                if len(result)>0 and result[-1]==n:
-                    result.pop()
-                else:
+                if not stack:
                     return False
-            ind+=1
-        if len(result)==0:
-            return True
-        else:
-            return False       
+                else:
+                    popped=stack.pop() 
+                    if popped!=hashmap[c]:
+                        return False
+        return not stack
